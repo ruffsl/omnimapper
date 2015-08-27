736a6c8498d517304904c6bad7e007493226870b
ruffsl <roxfoxpox@gmail.com>
2015-08-27 19:31:10 -0400

    Moving demo to base tag adding opencv cleaning nvidia

---
+++ docker/nvidia/run2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+docker run \
+	-it \
+	--env="DISPLAY" \
+	--env="QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--device /dev/nvidia0:/dev/nvidia0 \
+	--device /dev/nvidiactl:/dev/nvidiactl \
+	$@
