dafc6737220ee974866c94bec94c1c72faa07413
ruffsl <roxfoxpox@gmail.com>
2015-08-26 23:12:13 -0400

    Updating Dockerfiles Using official ros docker hub images moded OSRF using tags, not seperate repos removing dox image and adding gtsam cleaning dockerfiles bumping up gtsam to v3.2.1 updating make file tool

--- docker/demo/build.sh
+++ docker/demo/build.sh
@@ -1,25 +1,20 @@
+#!/bin/bash
+# set -e
 echo "if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi" >> ~/.bashrc
-source /opt/ros/indigo/setup.bash
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
 echo "export ROS_PACKAGE_PATH=/home/dox/omnimapper/ros/:${ROS_PACKAGE_PATH}" >> ~/.bash_aliases
-source ~/.bashrc
 
 source /opt/ros/indigo/setup.bash
-export ROS_PACKAGE_PATH=/home/dox/omnimapper/ros/:${ROS_PACKAGE_PATH}
-
-sudo rosdep init
-
-rosdep update
+export ROS_PACKAGE_PATH=/root/omnimapper/ros/:${ROS_PACKAGE_PATH}
 
 roscd omnimapper_ros
 
 rosmake
 
-# mkdir build 
+cd build
 
-cd build 
+cmake -DEIGEN_INCLUDE_DIRS="/root/gtsam/gtsam/3rdparty/Eigen" ..
 
-cmake -DEIGEN_INCLUDE_DIRS="/home/dox/gtsam/gtsam/3rdparty/Eigen" .. 
-
-make -j8
+make -j$(nproc)
