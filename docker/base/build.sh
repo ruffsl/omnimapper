24713c235b591afd0e2a469a491328974aaa4247
ruffsl <roxfoxpox@gmail.com>
2015-08-28 13:54:56 -0400

    Adding omnimapper_dev to base and moving base to later layer

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -4,10 +4,10 @@ echo "if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi" >> ~/.bashrc
 echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
-echo "export ROS_PACKAGE_PATH=/home/dox/omnimapper/ros/:${ROS_PACKAGE_PATH}" >> ~/.bash_aliases
+echo "export ROS_PACKAGE_PATH=/root/omnimapper_dev/ros/:${ROS_PACKAGE_PATH}" >> ~/.bash_aliases
 
 source /opt/ros/indigo/setup.bash
-export ROS_PACKAGE_PATH=/root/omnimapper/ros/:${ROS_PACKAGE_PATH}
+export ROS_PACKAGE_PATH=/root/omnimapper_dev/ros/:${ROS_PACKAGE_PATH}
 
 roscd omnimapper_ros
 
