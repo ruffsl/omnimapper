ec190cdaa846fd1e5b1887ac29832546213d45ff
ruffsl <roxfoxpox@gmail.com>
2015-08-29 16:14:04 -0400

    Building SVO with running tested

--- docker/nvidia/run2.sh
+++ docker/nvidia/run2.sh
@@ -5,6 +5,11 @@ docker run \
 	--env="DISPLAY" \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--volume="/home/ruffsl/Desktop/lol:/Desktop" \
 	--device /dev/nvidia0:/dev/nvidia0 \
 	--device /dev/nvidiactl:/dev/nvidiactl \
+	--device /dev/bus/usb:/dev/bus/usb \
+	--privileged \
 	$@
+
+# source /opt/ros/indigo/setup.bash
