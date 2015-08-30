88e5cc4a270ba4b2791a75a33fe6a275cfb428d3
ruffsl <roxfoxpox@gmail.com>
2015-08-29 21:57:38 -0400

    Changing dev branch

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -3,10 +3,12 @@
 echo "if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi" >> ~/.bashrc
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
-echo "export ROS_PACKAGE_PATH=/root/omnimapper_dev/ros/:${ROS_PACKAGE_PATH}" >> ~/.bash_aliases
+# echo "source /opt/ros/indigo/setup.bash" >> /root/.bashrc
+echo "source /root/catkin_ws/devel/setup.bash" >> /root/.bashrc
+echo "export ROS_PACKAGE_PATH=/root/omnimapper_dev/ros/:${ROS_PACKAGE_PATH}" >> /root/.bash_aliases
 
 source /opt/ros/indigo/setup.bash
+source /root/catkin_ws/devel/setup.bash
 export ROS_PACKAGE_PATH=/root/omnimapper_dev/ros/:${ROS_PACKAGE_PATH}
 
 roscd omnimapper_ros
