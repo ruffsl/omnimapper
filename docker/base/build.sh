9f5294a7c1c160fef6692eb3a6194b9add5fef83
ruffsl <roxfoxpox@gmail.com>
2015-09-17 19:56:11 -0400

    Touching up Dockerfiles and build scripts small personal changes should clean for PR though

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -3,9 +3,9 @@
 echo "if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi" >> ~/.bashrc
-# echo "source /opt/ros/indigo/setup.bash" >> /root/.bashrc
-echo "source /root/catkin_ws/devel/setup.bash" >> /root/.bashrc
-echo "export ROS_PACKAGE_PATH=/root/omnimapper_dev/ros/:${ROS_PACKAGE_PATH}" >> /root/.bash_aliases
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+echo "source /root/Desktop/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "export ROS_PACKAGE_PATH=/root/Desktop/omnimapper_dev/ros/:\${ROS_PACKAGE_PATH}" >> ~/.bashrc
 
 source /opt/ros/indigo/setup.bash
 source /root/catkin_ws/devel/setup.bash
--- docker/nvidia/run2.sh
+++ docker/nvidia/run2.sh
@@ -5,11 +5,10 @@ docker run \
 	--env="DISPLAY" \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-	--volume="/home/ruffsl/Desktop/lol:/Desktop" \
+	--volume="/home/ruffsl/Desktop/lol:/root/Desktop" \
 	--device /dev/nvidia0:/dev/nvidia0 \
 	--device /dev/nvidiactl:/dev/nvidiactl \
-	--device /dev/bus/usb:/dev/bus/usb \
-	--privileged \
+	--device /dev/bus/usb:/dev/bus/usb:rwm \
 	$@
 
 # source /opt/ros/indigo/setup.bash
