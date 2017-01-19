5f6fc9e711e39f87ea461b1c2ed267539fd62847
ruffsl <roxfoxpox@gmail.com>
2017-01-19 12:07:50 -0800

    make use of ROS_DISTRO env

--- docker/demo/build.sh
+++ docker/demo/build.sh
@@ -3,10 +3,10 @@
 echo "if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi" >> ~/.bashrc
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 echo "export ROS_PACKAGE_PATH=/home/dox/omnimapper/ros/:${ROS_PACKAGE_PATH}" >> ~/.bash_aliases
 
-source /opt/ros/indigo/setup.bash
+source /opt/ros/${ROS_DISTRO}/setup.bash
 export ROS_PACKAGE_PATH=/root/omnimapper/ros/:${ROS_PACKAGE_PATH}
 
 roscd omnimapper_ros
