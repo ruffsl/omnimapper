04cf7ef6ca1d9407155aded5e253e54e16503c17
ruffsl <roxfoxpox@gmail.com>
2017-01-19 13:00:15 -0800

    Updating dockerfiles removing deprecated maintainer layer

--- docker/nvidia/run.sh
+++ docker/nvidia/run.sh
@@ -1,45 +1,29 @@
 #!/bin/sh
 
-version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | tail -n 1)" == "$1"; }
-docker_version=$(docker version | grep 'Client version' | awk '{split($0,a,":"); print a[2]}' | tr -d ' ')
-# Docker 1.3.0 or later is required for --device
-if ! version_gt "${docker_version}" "1.2.0"; then
-	echo "Docker version 1.3.0 or greater is required"
-	exit 1
-fi
+# Add Xserver access for all local users, not ideal
+#xhost +local:root
 
-if test $# -lt 1; then
-	# Get the latest opengl-nvidia build
-	# and start with an interactive terminal enabled
-	args="-i -t $(docker images | grep ^cogrob/omnimapper-nvidia | head -n 1 | awk '{ print $1":"$2 }')"
-else
-        # Use this script with derived images, and pass your 'docker run' args
-	args="$@"
-fi
+# Create container with nvida devices and envs
+nvidia-docker create \
+  -it \
+  --env="DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  cogrob/omnimapper:nvidia
 
+# Remeber container details
+export containerId=$(docker ps -l -q)
+export containerConfigHostname=`docker inspect --format='{{ .Config.Hostname }}' $containerId`
 
-USER_UID=$(id -u)
-USER_GID=$(id -g)
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+# Add Xserver access for just this container
+xhost +local:$containerConfigHostname
 
-OMNIMAPER_ROOT="$(dirname "$(pwd)")"
-HOST_DESKTOP="/home/${USER}/Desktop"
+# Start container and execute an interative bash session
+docker start $containerId
+docker exec -it $containerId bash
 
-docker run \
-	-it --rm \
-	--volume=$HOST_DESKTOP:/home/dox/Desktop \
-	--volume=$OMNIMAPER_ROOT:/home/dox/git/omnimapper \
-	--volume=/run/user/${USER_UID}/pulse:/run/pulse \
-	--volume=$XSOCK:$XSOCK:rw \
-	--volume=$XAUTH:$XAUTH:rw \
-	--device /dev/nvidia0:/dev/nvidia0 \
-	--device /dev/nvidiactl:/dev/nvidiactl \
-	--env="XAUTHORITY=${XAUTH}" \
-	--env="USER_UID=${USER_UID}" \
-	--env="USER_GID=${USER_GID}" \
-	--env="DISPLAY=${DISPLAY}" \
-	-u dox \
-	$args
+# Remove Xserver access for just this container
+xhost -local:$containerConfigHostname
+
+# Remove Xserver access for all local users
+# xhost -local:root
